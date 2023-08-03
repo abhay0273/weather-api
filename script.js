53c0f98c84e6f4c5bff5b6fa7d9ce3f8b1e6ca95
abhay0273 <140791685+abhay0273@users.noreply.github.com>
2023-08-03 13:58:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let weather ={
+    "apikey":"e50c21ed9066277d920e17990bdd0efb",
+    fetchWeather: function(city) {
+
+        fetch(
+        //    "http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${id}"
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apikey  
+            )
+        
+          .then((response) => response.json())
+          .then((data) =>{  console.log(data);;this.displayWeather(data) });
+    },
+    displayWeather: function(data) {
+        // document.querySelector(".search").value= Name1;
+        const { name } = data;
+        const { icon, description } = data.weather[0] ;
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+         document.querySelector(".city").innerText = `Weather In ${name}`;
+         document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon  + ".png";      
+         document.querySelector(".description").innerText = description;
+         document.querySelector(".temp").innerText = temp  + "°C";
+         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+         document.querySelector(".wind").innerText = "Wind speed:" +  speed + "Km/h";
+         document.querySelector(".weather").classList.remove("loading");
+         document.body.style.backgroundImage = `url(https://source.unsplash.com/1600x900/?${name})`;
+    },
+    search: function(){
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+       document.querySelector(".search button").addEventListener("click", function(){
+       weather.search();
+ });
+
+    document.querySelector(".search-bar").addEventListener("keyup", function(event){
+        if (event.key == "Enter") {
+        weather.search();
+        }
+    });
+
+    weather.fetchWeather("");
